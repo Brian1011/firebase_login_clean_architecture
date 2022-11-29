8211f221d2de247d23a10ea9583e97a82c2d4e57
Brian Mutinda <mutindabrian26@gmail.com>
2022-11-29 08:31:51 +0300

    setup firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+//import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyBUm7ZGFnbJWboUdu7W7wlw_wrgafhCKYA',
+    appId: '1:706275468569:android:08613594c0ea00f1a15188',
+    messagingSenderId: '...',
+    projectId: 'clean-architecture-18a4e"',
+  ));
   runApp(const MyApp());
 }
 
@@ -13,15 +24,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
